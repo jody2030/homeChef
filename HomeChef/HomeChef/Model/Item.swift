91c8705108f893c38b729b1fcdc37c6dc9645682
Nojood Aljuaid <82294937+jody2030@users.noreply.github.com>
2023-06-18 18:41:21 +0300

    Add files via upload

---
+++ HomeChef/HomeChef/Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HomeChef
+//
+//  Created by Nojood Aljuaid  on 28/11/1444 AH.
+//
+
+import SwiftUI
+
+struct Item : Identifiable {
+    var id: String
+    var item_name: String
+    var item_cost: NSNumber
+    var item_details: String
+    var item_image: String
+    var item_ratings: String
+    var isAdded : Bool = false
+}
